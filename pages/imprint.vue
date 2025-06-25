5aacbceb113a46e9ecac3c8968c4f597f04b34ca
Julian Rabe <jr@tough.dev>
2025-06-25 18:08:12 +0200

    fix: add missing pages

---
+++ pages/imprint.vue
@@ -0,0 +1,5 @@
+<template xmlns="http://www.w3.org/1999/html">
+    <h1>Privacy policy</h1>
+    <p>Lorem ipsum blabla bla. This is a placeholder for the imprint. It will be filled in later.</p>
+</template>
+<script setup lang="ts"></script>
