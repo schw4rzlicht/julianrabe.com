7fabc5ca561ae533191554a96d8face2b2ebcd53
Julian Rabe <jr@tough.dev>
2025-06-25 18:13:07 +0200

    chore: typos

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,5 +1,5 @@
 <template xmlns="http://www.w3.org/1999/html">
-    <h1>Privacy policy</h1>
+    <h1>Impring</h1>
     <p>Lorem ipsum blabla bla. This is a placeholder for the imprint. It will be filled in later.</p>
 </template>
 <script setup lang="ts"></script>
