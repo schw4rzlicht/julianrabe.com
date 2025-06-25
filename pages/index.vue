73f4b82b1e58d39c10e020cb17b725363db382f6
Julian Rabe <jr@tough.dev>
2025-06-25 17:52:43 +0200

    feat: add portrait

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,10 @@
     <div class="sm:flex flex-row-reverse justify-between items-end">
         <div class="flex justify-center items-center sm:block">
             <img
-                class="mb-8 w-36 rounded-full object-cover shadow-lg grayscale"
-                src="https://avatars.dicebear.com/api/avataaars/.svg?b=%23606060&amp;w=400&amp;h=400&amp;top[]=bun&amp;topChance=100&amp;hairColor[]=brownDark&amp;accessoriesChance=0&amp;facialHair[]=beardLight&amp;facialHairChance=100&amp;facialHairColor[]=brownDark&amp;clothes[]=shirtVNeck&amp;clothesColor[]=black&amp;eyes[]=squint&amp;eyebrow[]=defaultNatural&amp;mouth[]=default&amp;skin[]=light"
-                alt=""
+                src="/img/me-144px@2.png"
+                class="mb-8 w-36 rounded-full object-cover shadow-lg"
+                alt="Portrait"
+                srcset="/img/me-144px.png, /img/me-144px@2.png 2x"
             />
         </div>
         <h1>Hi!</h1>
