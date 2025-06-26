a8fd027ef3c5c092b9e882d1a051c9ebc567799b
Julian Rabe <jr@tough.dev>
2025-06-26 09:07:02 +0200

    refactor: improve structure

---
+++ components/content/Summary.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="sm:flex flex-row-reverse justify-between items-end">
+        <div class="flex justify-center items-center sm:block">
+            <img
+                src="/assets/img/me-144px@2.png"
+                class="mb-8 w-36 rounded-full object-cover shadow-lg"
+                alt="Portrait"
+                srcset="/assets/img/me-144px.png 1x, /assets/img/me-144px@2.png 2x"
+            />
+        </div>
+        <h1>Hi!</h1>
+    </div>
+    <h2>My name is <Highlight>Julian</Highlight>, I am a full stack web engineer from Germany.</h2>
+    <p>
+        My mission is to create and develop <Highlight>scalable</Highlight>, <Highlight>beautiful</Highlight> web
+        applications that are actually useful.
+    </p>
+    <p>
+        I strive for <Highlight>top-notch</Highlight> technology and always try to get the most out of it to
+        <Highlight>benefit users</Highlight>.
+    </p>
+    <p>If I need something new to complete the mission, I will <Highlight>learn it</Highlight>.</p>
+    <p>
+        Of course this needs <Highlight>listening</Highlight>, <Highlight>planning</Highlight> and
+        <Highlight>building</Highlight> custom solutions that fit the purpose — not more, not less.
+    </p>
+    <p class="pt-4">
+        Got your attention? Get to know me better <NuxtLink to="/about">here</NuxtLink> or dive right into my stack
+        <NuxtLink to="/stack">here</NuxtLink>. An excerpt of my work can be found <NuxtLink to="/work">here</NuxtLink>.
+    </p>
+</template>
+
+<script setup lang="ts">
+import Highlight from "~/components/Highlight.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,5 @@
 <template>
-    <div class="sm:flex flex-row-reverse justify-between items-end">
-        <div class="flex justify-center items-center sm:block">
-            <img
-                src="/img/me-144px@2.png"
-                class="mb-8 w-36 rounded-full object-cover shadow-lg"
-                alt="Portrait"
-                srcset="/img/me-144px.png, /img/me-144px@2.png 2x"
-            />
-        </div>
-        <h1>Hi!</h1>
-    </div>
-    <h2>My name is <Highlight>Julian</Highlight>, I am a full stack web engineer from Germany.</h2>
-    <p>
-        My mission is to create and develop <Highlight>scalable</Highlight>, <Highlight>beautiful</Highlight> web
-        applications that are actually useful.
-    </p>
-    <p>
-        I strive for <Highlight>top-notch</Highlight> technology and always try to get the most out of it to
-        <Highlight>benefit users</Highlight>.
-    </p>
-    <p>If I need something new to complete the mission, I will <Highlight>learn it</Highlight>.</p>
-    <p>
-        Of course this needs <Highlight>listening</Highlight>, <Highlight>planning</Highlight> and
-        <Highlight>building</Highlight> custom solutions that fit the purpose — not more, not less.
-    </p>
-    <p class="pt-4">
-        Got your attention? Get to know me better <NuxtLink to="/about">here</NuxtLink> or dive right into my stack
-        <NuxtLink to="/stack">here</NuxtLink>. An excerpt of my work can be found <NuxtLink to="/work">here</NuxtLink>.
-    </p>
+    <ContentSummary />
 </template>
 
-<script setup lang="ts">
-import Highlight from "~/components/Highlight.vue";
-</script>
+<script setup lang="ts"></script>
