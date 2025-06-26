a685adf1e8cadc8537d8779087c59de0b5433387
Julian Rabe <jr@tough.dev>
2025-06-26 09:15:30 +0200

    refactor: move global css to tailwind.css

--- App.vue
+++ App.vue
@@ -11,26 +11,3 @@
         </div>
     </div>
 </template>
-
-<style>
-a {
-    @apply text-pink-500 underline underline-offset-2;
-}
-
-body {
-    @apply bg-slate-900;
-}
-
-h1,
-h2 {
-    @apply font-bold;
-}
-
-h1 {
-    @apply text-4xl sm:text-8xl text-pink-500 pb-4 sm:pb-8;
-}
-
-h2 {
-    @apply text-2xl sm:text-4xl text-lime-400 pb-3 sm:pb-6;
-}
-</style>
